5c8a86eb968023ee4d8dec6a9bd4aabe0e09a043
Emin1905 <cetinemin40@gmail.com>
2023-04-16 23:41:42 +0200

    fleet functionality second user story added

--- src/test/java/com/cydeo/pages/FleetFunctionality_Page.java
+++ src/test/java/com/cydeo/pages/FleetFunctionality_Page.java
@@ -1,6 +1,7 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -44,5 +45,29 @@ public class FleetFunctionality_Page {
 
     @FindBy(xpath = "//a[@data-menu-xmlid='fleet.menu_root']")
     public WebElement fleetPageInMenuMore;
+
+    @FindBy(xpath = "//input[@id='o_field_input_74']")
+    public WebElement vehicleChoice;
+
+    @FindBy(xpath = "//a[.='Bmw/Serie 1/1-BMW-001']")
+    public WebElement bmwOption;
+
+    @FindBy(xpath = "//input[@id='o_field_input_75']")
+    public WebElement typeChoice;
+
+    @FindBy(xpath = "//a[.='Leasing']")
+    public WebElement leasingChoice;
+
+    @FindBy(xpath = "//input[@id='o_field_input_76']")
+    public WebElement activationCost;
+
+    @FindBy(xpath = "//select[@id='o_field_input_78']")
+    public WebElement recurringCostAmount;
+
+    @FindBy(xpath = "//input[@id='o_field_input_77']")
+    public WebElement recurringCostInput;
+
+
+
 
 }
