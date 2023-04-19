2f50d38caaa44ad6ea7f4b2ddc8944ce02832fb8
Mokhigul <moxigul.sadullayeva@mail.ru>
2023-04-19 14:45:45 +0200

    surveys functionality second scenario added

--- src/test/java/com/cydeo/step_definitions/surveysMokhi_stepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/surveysMokhi_stepDefinitions.java
@@ -1,38 +1,70 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.SurveysMokhi_Page;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class surveysMokhi_stepDefinitions {
 
+    SurveysMokhi_Page surveysMokhi_page = new SurveysMokhi_Page();
+
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
     }
 
     @Given("user logins by entering valid username {string} and valid password {string}")
     public void user_logins_by_entering_valid_username_and_valid_password(String string, String string2) {
 
+        surveysMokhi_page.usernameInput.sendKeys(string);
+        surveysMokhi_page.passwordInput.sendKeys(string2);
+
     }
 
     @Then("by clicking the login button user goes to the homepage")
     public void by_clicking_the_login_button_user_goes_to_the_homepage() {
 
+        surveysMokhi_page.loginBtn.click();
+        Assert.assertTrue(surveysMokhi_page.homePageMsg.isDisplayed());
     }
 
     @Then("user clicks More from the menu bar and selects Surveys")
     public void user_clicks_more_from_the_menu_bar_and_selects_surveys() {
+
+        surveysMokhi_page.moreBtn.click();
+        surveysMokhi_page.surveysBtn.click();
 
     }
 
     @Then("user goes to Surveys page and clicks on Create button")
     public void user_goes_to_surveys_page_and_clicks_on_create_button() {
 
+        surveysMokhi_page.createBtn.click();
+
     }
 
     @Then("user should see new survey form")
     public void user_should_see_new_survey_form() {
 
+        Assert.assertTrue(surveysMokhi_page.surveyTitle.isDisplayed());
     }
 
+    @When("user leaves the Survey Title field empty")
+    public void userLeavesTheSurveyTitleFieldEmpty() {
+
+        surveysMokhi_page.surveyTitle.sendKeys("");
+        surveysMokhi_page.saveBtn.click();
+    }
+
+    @Then("user should see an error message")
+    public void userShouldSeeAnErrorMessage() {
+
+        Assert.assertTrue(surveysMokhi_page.errorMsg.isDisplayed());
+    }
 }
--- src/test/resources/features/surveysFunctionality_Mokhi.feature
+++ src/test/resources/features/surveysFunctionality_Mokhi.feature
@@ -2,7 +2,7 @@ Feature: Surveys Functionality
   User Story:
   As a Posmanager, I should be able to create and design a new survey from "Surveys" module
 
-  @wip
+
   Scenario: Verify that User can reach New Survey Form by clicking Surveys --> Create
     Given user is on the login page
     And user logins by entering valid username "posmanager10@info.com" and valid password "posmanager"
@@ -10,6 +10,19 @@ Feature: Surveys Functionality
     And user clicks More from the menu bar and selects Surveys
     And user goes to Surveys page and clicks on Create button
     Then user should see new survey form
+
+  @wip
+    Scenario: Verify that if Survey Title field leaves blank, an error message "The following fields are
+    invalid:" is appeared.
+      Given user is on the login page
+      And user logins by entering valid username "posmanager10@info.com" and valid password "posmanager"
+      Then by clicking the login button user goes to the homepage
+      And user clicks More from the menu bar and selects Surveys
+      And user goes to Surveys page and clicks on Create button
+      And user should see new survey form
+      When user leaves the Survey Title field empty
+      Then user should see an error message
+
 
 
 
